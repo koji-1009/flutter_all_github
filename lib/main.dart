28c2bdf6630f58aa56e50d1b9d9ff403d2291799
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-06 00:11:20 +0900

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_all_github/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
