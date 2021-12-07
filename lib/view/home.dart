761e3d345125601e168a3e11e254f89e9a53f897
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-08 01:08:27 +0900

    Add AdaptiveNavigationScaffold

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,6 @@
+import 'package:adaptive_navigation/adaptive_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends HookConsumerWidget {
@@ -6,9 +8,21 @@ class HomePage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
+    final index = useState(0);
+
+    return AdaptiveNavigationScaffold(
+      appBar: AdaptiveAppBar(
         title: const Text('Home'),
+      ),
+      selectedIndex: index.value,
+      onDestinationSelected: (value) => index.value = value,
+      destinations: const [
+        AdaptiveScaffoldDestination(title: 'Alarm', icon: Icons.alarm),
+        AdaptiveScaffoldDestination(title: 'Book', icon: Icons.book),
+        AdaptiveScaffoldDestination(title: 'Cake', icon: Icons.cake),
+      ],
+      body: const Center(
+        child: Text('test'),
       ),
     );
   }
