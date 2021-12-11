5aac220bc25e4bdeac74869e1b5147d318ae6e4c
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-11 15:18:21 +0900

    Add main body contents

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+const _body = [
+  _AlarmBody(),
+  _BookBody(),
+  _CakeBody(),
+];
+
 class HomePage extends HookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -21,9 +27,40 @@ class HomePage extends HookConsumerWidget {
         AdaptiveScaffoldDestination(title: 'Book', icon: Icons.book),
         AdaptiveScaffoldDestination(title: 'Cake', icon: Icons.cake),
       ],
-      body: const Center(
-        child: Text('test'),
-      ),
+      body: _body[index.value],
+    );
+  }
+}
+
+class _AlarmBody extends StatelessWidget {
+  const _AlarmBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Alarm'),
+    );
+  }
+}
+
+class _BookBody extends StatelessWidget {
+  const _BookBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Book'),
+    );
+  }
+}
+
+class _CakeBody extends StatelessWidget {
+  const _CakeBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Cake'),
     );
   }
 }
