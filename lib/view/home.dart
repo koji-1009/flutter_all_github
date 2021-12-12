fac8a2298b60f0f45ce8b20a19053d6b39eae052
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-12 20:21:29 +0900

    Make tab classes

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,13 +1,10 @@
 import 'package:adaptive_navigation/adaptive_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_all_github/view/tabs/makes.dart';
+import 'package:flutter_all_github/view/tabs/reviews.dart';
+import 'package:flutter_all_github/view/tabs/settings.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-const _body = [
-  _AlarmBody(),
-  _BookBody(),
-  _CakeBody(),
-];
 
 class HomePage extends HookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +12,13 @@ class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final index = useState(0);
+    final body = useMemoized(
+      () => const [
+        MakesBody(),
+        ReviewsBody(),
+        SettingsBody(),
+      ],
+    );
 
     return AdaptiveNavigationScaffold(
       appBar: AdaptiveAppBar(
@@ -23,44 +27,20 @@ class HomePage extends HookConsumerWidget {
       selectedIndex: index.value,
       onDestinationSelected: (value) => index.value = value,
       destinations: const [
-        AdaptiveScaffoldDestination(title: 'Alarm', icon: Icons.alarm),
-        AdaptiveScaffoldDestination(title: 'Book', icon: Icons.book),
-        AdaptiveScaffoldDestination(title: 'Cake', icon: Icons.cake),
+        AdaptiveScaffoldDestination(
+          title: 'Makes',
+          icon: Icons.work,
+        ),
+        AdaptiveScaffoldDestination(
+          title: 'Reviews',
+          icon: Icons.list,
+        ),
+        AdaptiveScaffoldDestination(
+          title: 'Settings',
+          icon: Icons.settings,
+        ),
       ],
-      body: _body[index.value],
-    );
-  }
-}
-
-class _AlarmBody extends StatelessWidget {
-  const _AlarmBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Alarm'),
-    );
-  }
-}
-
-class _BookBody extends StatelessWidget {
-  const _BookBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Book'),
-    );
-  }
-}
-
-class _CakeBody extends StatelessWidget {
-  const _CakeBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cake'),
+      body: body[index.value],
     );
   }
 }
