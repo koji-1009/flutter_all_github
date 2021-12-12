7e058f6a6657d14aecdd5f4d7a2575e5642c2998
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-12-12 12:59:18 +0900

    Add and setup ferry

--- lib/model/entity/app_state.freezed.dart
+++ lib/model/entity/app_state.freezed.dart
@@ -1,6 +1,5 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
 
 part of 'app_state.dart';
 
@@ -119,14 +118,15 @@ class _$_AppState with DiagnosticableTreeMixin implements _AppState {
   @override
   bool operator ==(dynamic other) {
     return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _AppState &&
-            const DeepCollectionEquality().equals(other.username, username));
+        (other is _AppState &&
+            (identical(other.username, username) ||
+                const DeepCollectionEquality()
+                    .equals(other.username, username)));
   }
 
   @override
   int get hashCode =>
-      Object.hash(runtimeType, const DeepCollectionEquality().hash(username));
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(username);
 
   @JsonKey(ignore: true)
   @override
@@ -138,7 +138,7 @@ abstract class _AppState implements AppState {
   const factory _AppState({String username}) = _$_AppState;
 
   @override
-  String get username;
+  String get username => throw _privateConstructorUsedError;
   @override
   @JsonKey(ignore: true)
   _$AppStateCopyWith<_AppState> get copyWith =>
